d1248848acd2d761ea39a1492baf11de0e2ec353
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-08-01 17:23:03 +0000

    nearly finished

--- src/components/upload/upload.component.js
+++ src/components/upload/upload.component.js
@@ -1,14 +1,16 @@
 import { React, useState, useEffect } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import Multiselect from "multiselect-react-dropdown";
 import Type from "./type.selectDropdown.component";
+import axios from "axios";
 
 function Upload() {
   const [type, setType] = useState([]);
   const [subtype, setSubtype] = useState([]);
-  const [selectType, setSelectType] = useState();
-  const [selectSubType, setSelectSubType] = useState([]);
-  const [submit, setSumbit] = useState(false);
+  const [selectIdType, setSelectIdType] = useState();
+  const [selectIdSubType, setSelectIdSubType] = useState([]);
+  const [namefood, setNamefood] = useState("");
+  const [image, setImage] = useState(null);
   const fetchData = async () => {
     const fetching = async (type) => {
       const responese = await fetch(`http://localhost:4000/${type}`);
@@ -26,22 +28,63 @@ function Upload() {
     fetchData();
   }, []);
 
+  const post = () =>{
+    const data = new FormData();
+    data.append("photos",image);
+    data.append("namefood",namefood);
+    data.append("idtype",selectIdType);
+    data.append("idsubtype",selectIdSubType);
+
+  }
+
   return (
     <Form>
-      <Form.Label>Type</Form.Label>
-      <Type type={type} setOutput={setSelectType} />
-      <Form.Label>Subtype</Form.Label>
-      <Multiselect
-        options={subtype}
-        displayValue="name"
-        closeIcon="close"
-        onSelect={(selectedList, selectedItem) => {
-          setSelectSubType(selectedList);
-        }}
-        onRemove={(selectedList, removedItem) => {
-          setSelectSubType(selectedList);
-        }}
-      />
+      <Form.Group>
+        <Form.Label>ชื่ออาหาร</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="ชื่ออาหาร"
+          onChange={(e) => {
+            setNamefood(e.target.value);
+          }}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>เลือกรูปอาหาร</Form.Label>
+        <Form.Control
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            setImage(e.target.files[0]);
+          }}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Type</Form.Label>
+        <Type type={type} setOutput={setSelectIdType} />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Subtype</Form.Label>
+        <Multiselect
+          options={subtype}
+          displayValue="name"
+          closeIcon="close"
+          onSelect={(selectedList, selectedItem) => {
+
+            const result = selectedList.map((data,index)=>{
+              return [...data._id]
+
+            })
+            console.log(result);
+
+          }}
+          onRemove={(selectedList, removedItem) => {
+            const result = selectedList.map((data,index)=>{return [...data._id]})
+            console.log(result);
+          }}
+        />
+        <Button type="submit" onClick={post}>Sumbit</Button>
+      </Form.Group>
     </Form>
   );
 }
