083676d0594a7d457f48854c31d0b4dc199671d4
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-08-07 15:59:31 +0700

    fix height's image card

--- src/components/home/foodCard/foodCard.component.js
+++ src/components/home/foodCard/foodCard.component.js
@@ -1,11 +1,12 @@
-import { React } from "react";
+import { React , useState} from "react";
 import PropTypes from "prop-types";
 import { Card, Container } from "react-bootstrap";
 
 function FoodCard({ cardStyleWidth, cardMargin, cardVariant, image, title, type, subtype }) {
+  const [width] = useState(window.innerWidth);
   return (
     <Card style={{ width: cardStyleWidth , marginLeft:cardMargin , marginRight:cardMargin}} className="justify-content-center">
-      <Card.Img src={image} variant={cardVariant} style={{height:"240px"}} />
+      <Card.Img src={image} variant={cardVariant} style={{height:width <= 767 ? `${width*0.6}px` : "240px"}}/>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Subtitle>ประเภท: {type}</Card.Subtitle>
